1cae5306bcba81902dcd7400019bc45b29e83e6c
larso <larsonan@umich.edu>
2025-03-25 21:52:51 -0400

    Make clean file correct

--- clean.sh
+++ clean.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 rm zwyx.cpp
-rm Tests
+rm README.md
+rm Tests/helloworld.zwyx
+rm Tests/helloworld_expected.asm
+rm Tests/fizzbuzz.zwyx
+rm Tests/fizzbuzz_expected.asm
+rm Tests/update.zwyx
+rm Tests/update_expected.asm
+rm Tests/shapes.zwyx
+rm Tests/shapes_expected.asm
+rm Tests/temperature.zwyx
+rm Tests/temperature_expected.asm
+rm Tests/tictactoe.zwyx
+rm Tests/tictactoe_expected.asm
+rm Tests/first_test.zwyx
+rm Tests/first_test_expected.asm
+rm Tests/utilities_lib.zwyx
+rm Tests/utilities_lib_2.zwyx
+rm Tests/sysapi_elf64.zwyx
+rm Tests/sysapi_elf64_2.zwyx
+rm Tests/sysapi_macho64.zwyx
+rm Tests/sysapi.zwyx
+rm Tests/test.sh
+rm Tests/test.cpp
 
